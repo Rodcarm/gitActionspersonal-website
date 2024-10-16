4897829b1d4169f6316dc50d3233590bd4f6f777
Rodrigo <rcarmona@realmetrics.io>
2024-10-16 17:23:58 +0000

    THIS IS A TEST

--- webpersonal/portfolio/views.py
+++ webpersonal/portfolio/views.py
@@ -4,7 +4,7 @@ from .models import Project
 # Create your views here.
 
 def portfolio(request):
-    print("this is portfolio/views.py")
+    print("this is other test2")
     projects = Project.objects.all()
     return render(request,"portfolio/portfolio.html", {'projects':projects})
 
