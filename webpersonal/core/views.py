0a11312ab20087e506d1755939946c5017715968
Rodrigo <rcarmona@realmetrics.io>
2024-10-16 03:55:03 +0000

    Agregar archivo específico

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    print("view core")
     return render(request, "core/home.html")
 
 def about(request):
+    print("ttttt")
     return render(request,"core/about.html")
 
 
 def contact(request):
+    print("dest")
     return render(request,"core/contact.html")
